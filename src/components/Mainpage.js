4c9800f61dba7ed24bfb9ec17ff858c2919d0c29
XDeviation <191602098@qq.com>
2022-01-08 21:04:26 +0800

    feat: add CNAME and router

---
+++ src/components/Mainpage.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {
+    Timeline
+} from "antd"; 
+
+class Mainpage extends React.Component {
+  render() {
+    return (
+      <div>
+        <Timeline>
+          <Timeline.Item>Create a services site 2015-09-01</Timeline.Item>
+          <Timeline.Item>Solve initial network problems 2015-09-01</Timeline.Item>
+          <Timeline.Item>Technical testing 2015-09-01</Timeline.Item>
+          <Timeline.Item>Network problems being solved 2015-09-01</Timeline.Item>
+        </Timeline>
+      </div>
+    );
+  }
+}
+
+export default Mainpage;
